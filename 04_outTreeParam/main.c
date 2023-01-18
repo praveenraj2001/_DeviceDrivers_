7c52939faa8c467ade509908fb3c0747dd89169e
praveenraj <nyalam.praveenraj@gmail.com>
2023-01-18 14:09:16 +0530

    Add files via upload

---
+++ 04_outTreeParam/main.c
@@ -0,0 +1,21 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+int ndevice = 1;
+module_param(ndevice,int,S_IRUGO);
+static int __init hello_init(void)
+{
+printk("Hello, world\n ndevice = %d\n",ndevice);
+return 0;
+}
+static void __exit hello_exit(void)
+{
+printk("Goodbye, cruel world\n");
+}
+module_init(hello_init);
+module_exit(hello_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Praveenraj");
+MODULE_DESCRIPTION("A Simple Module");
